381cf46d3010b1465e8c7d4536263c302a028095
Marcel de Wit <marcel.dewit@teamrockstars.nl>
2020-01-30 16:28:46 +0100

    Added WIP DiceRoller microservice

---
+++ DiceRoller/DiceRoller/Controllers/DiceRollController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiceRoller.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DiceRollController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            var random = new Random();
+            return random.Next(1, 7).ToString();
+        }
+    }
+}
