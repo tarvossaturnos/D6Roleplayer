96b88fcdc1d6cd415a5f210aaf4974727861ab31
Marcel de Wit <marcel.dewit@teamrockstars.nl>
2020-02-10 09:15:40 +0100

    Session info WIP

--- D6Roleplayer/D6Roleplayer.Web/Hubs/DiceRollHub.cs
+++ D6Roleplayer/D6Roleplayer.Web/Hubs/DiceRollHub.cs
@@ -3,12 +3,17 @@ using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http.Connections;
 using D6Roleplayer.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace D6Roleplayer.Web.Hubs
 {
     public class DiceRollHub : Hub
     {
         private readonly IDiceRollService diceRollService;
+
+        private static List<string> connections = new List<string>();
 
         public DiceRollHub(IDiceRollService diceRollService)
         {
@@ -17,8 +22,20 @@ namespace D6Roleplayer.Web.Hubs
 
         public override async Task OnConnectedAsync()
         {
-            await Groups.AddToGroupAsync(Context.ConnectionId, GetRoleplaySessionId());
+            var roleplaySessionId = GetRoleplaySessionId();
+            await Groups.AddToGroupAsync(Context.ConnectionId, roleplaySessionId);
+            connections.Add(roleplaySessionId);
+
             await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            var roleplaySessionId = GetRoleplaySessionId();
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roleplaySessionId);
+            connections.Remove(roleplaySessionId);
+
+            await base.OnDisconnectedAsync(exception);
         }
 
         public async Task RequestDiceRoll(string username, string message, string count)
@@ -78,6 +95,13 @@ namespace D6Roleplayer.Web.Hubs
         public async Task RequestResetDrawing()
         {
             await Clients.Group(GetRoleplaySessionId()).SendAsync("ResetDrawing");
+        }
+
+        public async Task UpdateUserCount()
+        {
+            var roleplaySessionId = GetRoleplaySessionId();
+            await Clients.Group(GetRoleplaySessionId()).SendAsync("UpdateUserCount",
+                connections.Where(connection => connection == roleplaySessionId).Count());
         }
 
         private string GetRoleplaySessionId()
--- D6Roleplayer/D6Roleplayer.Web/src/js/signalr-base.js
+++ D6Roleplayer/D6Roleplayer.Web/src/js/signalr-base.js
@@ -5,8 +5,21 @@ const id = params.get('sessionId');
 var connection = new signalR.HubConnectionBuilder().withUrl(`/diceRollHub?sessionId=${id}`).build();
 
 connection.start().then(function () {
+    document.getElementById("sessionConnected").textContent = "Connected";
     document.getElementById("sendButton").disabled = false;
+    connection.invoke("UpdateUserCount");
 }).catch(function (err) {
     window.alert("Could not connect to the session. Please reload the page.")
     return console.error(err.toString());
+});
+
+connection.onclose(function() {
+    document.getElementById("sessionConnected").textContent = "Not connected";    
+    connection.invoke("UpdateUserCount");
+});
+
+connection.on("UpdateUserCount", function (amount) {
+    var userCount = parseInt(document.getElementById('userCount').innerHTML);
+    userCount = amount;    
+    document.getElementById('userCount').innerHTML = userCount;
 });
